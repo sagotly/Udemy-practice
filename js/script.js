f878f595262bef0ba31d6ae1a1e62f62f78c8359
Tolik <laktolik9o@gmail.com>
2022-11-19 19:32:28 +0200

    second practice

--- js/script.js
+++ js/script.js
@@ -1,21 +1,64 @@
 'use strict';
-// let num = 50;
 
-// for (let i = 1; i < 10; i++) {
-//       console.log(i);
-//       for (let c = 1; c < 10; c++) {
-//             console.log(c);
-//       }
-// }
+const numberOfNumbers = +prompt('how many movies do u watched', '');
 
-let result = '',
-      leng = 10,
-      x = 0; 
-while (x < leng) {
-      for (let i= 0; i < x; ) {
-            result += '*';
-      
-      }
-      result += '\n';
-      x++
+const personaMoviesDB = {
+      count: numberOfNumbers,
+      movies: {},
+      actors: {},
+      genres: [],
+      privat: false
+};
+
+for (let i = 0; i < 2; i++){
+    let x = prompt('what is one of your last films?' , '');
+    let y = +prompt('how would you rate it?' , '');
+    if (x == null || y == 0 || x.length == 0 || y == null || x.length > 50) {
+        i--;
+    } else{
+    personaMoviesDB.movies[x] = y;
+    }
 }
+
+
+
+// /* let i = 0;
+//
+// while(i < 2) {
+//     let x = prompt('what is one of your last films?' , '');
+//     let y = +prompt('how would you rate it?' , '');
+//     if (x.length == 0 || y == 0) {
+//         i--;
+//     } else{
+//     personaMoviesDB.movies[x] = y;
+//     }
+//     i++;
+// } */
+//
+// /* let i = 0;
+// do {
+// let x = prompt('what is one of your last films?' , '');
+//     let y = +prompt('how would you rate it?' , '');
+//     if (x.length == 0 || y == 0) {
+//         i--;
+//     } else{
+//     personaMoviesDB.movies[x] = y;
+//     }
+//     i++;
+// }
+// while(i < 2); */
+
+
+
+
+if (personaMoviesDB.count <= 10) {
+    console.log('you wachted few movies');
+} else if(personaMoviesDB.count > 10 && personaMoviesDB.count <= 20) {
+    console.log('you are normal wachter');
+} else if(personaMoviesDB.count > 20) {
+    console.log("touch grass");
+} else {
+    console.log('error');
+}
+
+console.log(personaMoviesDB);
