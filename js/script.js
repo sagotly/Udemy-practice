10ef832b272a90bdb52e97aedd6ba77be5a37734
Tolik <laktolik9o@gmail.com>
2022-11-22 21:18:10 +0200

    practice #2

--- js/script.js
+++ js/script.js
@@ -1,28 +1,75 @@
 'use strict';
 
-const numberOfNumbers = +prompt('how many movies do u watched', '');
+let numberOfFilms;
+
+function start() {
+   numberOfFilms = +prompt('how many movies do u watched', '');
+   while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('how many movies do u watched', '');
+   }
+}
+start();
 
 const personaMoviesDB = {
-      count: numberOfNumbers,
+      count: numberOfFilms,
       movies: {},
       actors: {},
       genres: [],
       privat: false
 };
 
-for (let i = 0; i < 2; i++){
-    let x = prompt('what is one of your last films?' , '');
-    let y = +prompt('how would you rate it?' , '');
-    if (x == null || y == '' || x.length == 0 || y == null || x.length > 50) {
-        i--;
-    } else{
-    personaMoviesDB.movies[x] = y;
+function rememberMyFilms(films) {
+    for (let i = 0; i < 2; i++){
+        let x = prompt('what is one of your last films?' , '');
+        let y = +prompt('how would you rate it?' , '');
+        if (x == null || y == '' || x.length == 0 || y == null || x.length > 50) {
+            i--;
+        } else{
+        films[x] = y;
+        }
     }
 }
+rememberMyFilms(personaMoviesDB.movies);
+
+function personalLevel(count) {
+    if (count <= 10) {
+        console.log('you wachted few movies');
+    } else if(count > 10 && count <= 20) {
+        console.log('you are normal wachter');
+    } else if(count > 20) {
+        console.log("touch grass");
+    } else {
+        console.log('error');
+    }
+}
+personalLevel(personaMoviesDB.count);
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personaMoviesDB);
+    }
+}
+showMyDB(personaMoviesDB.privat);
+
+function writeYoursGenres(genres) {
+    for (let i = 0; i < 3; i++) {
+        genres[i] = prompt(`whats your top ${i + 1} genre?`, '');
+    }
+}
+writeYoursGenres(personaMoviesDB.genres);
 
 
 
-// /* let i = 0;
+
+
+
+
+
+
+
+
+
+// let i = 0;
 //
 // while(i < 2) {
 //     let x = prompt('what is one of your last films?' , '');
@@ -33,9 +80,9 @@ for (let i = 0; i < 2; i++){
 //     personaMoviesDB.movies[x] = y;
 //     }
 //     i++;
-// } */
+// }
 //
-// /* let i = 0;
+// let i = 0;
 // do {
 // let x = prompt('what is one of your last films?' , '');
 //     let y = +prompt('how would you rate it?' , '');
@@ -46,19 +93,13 @@ for (let i = 0; i < 2; i++){
 //     }
 //     i++;
 // }
-// while(i < 2); */
-
-
-
-
-if (personaMoviesDB.count <= 10) {
-    console.log('you wachted few movies');
-} else if(personaMoviesDB.count > 10 && personaMoviesDB.count <= 20) {
-    console.log('you are normal wachter');
-} else if(personaMoviesDB.count > 20) {
-    console.log("touch grass");
-} else {
-    console.log('error');
-}
-
-console.log(personaMoviesDB);
+// while(i < 2);
+// for (let i = 0; i < 2; i++){
+//     let x = prompt('what is one of your last films?' , '');
+//     let y = +prompt('how would you rate it?' , '');
+//     if (x == null || y == '' || x.length == 0 || y == null || x.length > 50) {
+//         i--;
+//     } else{
+//     personaMoviesDB.movies[x] = y;
+//     }
+// }
